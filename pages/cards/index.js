7ce5e2a6a18bb3ff2dda67f3bb84cdbb9ced8628
maxcramer <maximiliancramer@yahoo.com>
2022-04-28 11:03:17 +0100

    logging data to console from api call

---
+++ pages/cards/index.js
@@ -0,0 +1,20 @@
+import {useState} from 'react';
+
+
+
+function CardsPage (props) {
+    return (
+        <div>
+            <h2>Trying to map over each item</h2>
+            {/* {props.map(prop => {
+                return (
+                    <div key={prop.item_id}>
+                        <h3>Test</h3>
+                    </div>
+                )
+            })} */}
+        </div>
+    )
+}
+
+export default CardsPage;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
 import SocialCardsList from '../components/socialCards/SocialCardsList'
+import CardsPage from './cards/index';
+
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const defaultEndpoint = 'http://private-cc77e-aff.apiary-mock.com/posts';
+export async function getServerSideProps() {
+  const res = await fetch(defaultEndpoint);
+  const data = await res.json();
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+export default function Home({ data }) {
+  console.log(data)
   return (
-    <SocialCardsList  socialCards={ name='pete' }/>
+    // <SocialCardsList  socialCards={ name='pete' }/>
+    <CardsPage />
   )
 }
