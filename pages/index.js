bea67c34c048b7fe5f6140bee00ca500186b317a
maxcramer <maximiliancramer@yahoo.com>
2022-04-28 11:37:30 +0100

    passing data from index.js to socialcardslist.js componet

--- pages/index.js
+++ pages/index.js
@@ -7,18 +7,21 @@ const defaultEndpoint = 'http://private-cc77e-aff.apiary-mock.com/posts';
 export async function getServerSideProps() {
   const res = await fetch(defaultEndpoint);
   const data = await res.json();
+  console.log("This is the data", data)
   return {
     props: {
-      data: {
-        account_name: data.account_name,
-        id: data.item_id,
-      }
+      data
+      //  {
+      //   name: data.account_name,
+      //   id: data.item_id,
+      // }
     }
   }
 }
 
-export default function Home({ props }) {
+export default function Home({ data }) {
+  console.log(data)
   return (
-    <SocialCardsList items={props} />
+    <SocialCardsList items={data} />
   )
 }
